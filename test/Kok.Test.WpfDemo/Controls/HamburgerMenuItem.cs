e65601853dd4812559317253a91d775d36db7f62
haochuang <haochuang@newunited.com.cn>
2024-05-11 17:47:35 +0800

    修改Item样式

--- test/Kok.Test.WpfDemo/Controls/HamburgerMenuItem.cs
+++ test/Kok.Test.WpfDemo/Controls/HamburgerMenuItem.cs
@@ -9,4 +9,13 @@ public class HamburgerMenuItem : RadioButton
     {
         DefaultStyleKeyProperty.OverrideMetadata(typeof(HamburgerMenuItem), new FrameworkPropertyMetadata(typeof(HamburgerMenuItem)));
     }
+
+    public string Text
+    {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+
+    public static readonly DependencyProperty TextProperty =
+         DependencyProperty.Register(nameof(Text), typeof(string), typeof(HamburgerMenuItem), new PropertyMetadata(string.Empty));
 }
