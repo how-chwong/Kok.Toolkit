25d03a5123d43990f7ade6b000f355e15b624362
haochuang <haochuang@newunited.com.cn>
2024-07-11 15:32:04 +0800

    update bat

--- publish.bat
+++ publish.bat
@@ -4,7 +4,7 @@ set PUBLISH_PATH=.\publish
 
 dotnet pack .\src\Kok.Toolkit.Core\Kok.Toolkit.Core.csproj -c Release -o %PUBLISH_PATH%
 dotnet pack .\src\Kok.Toolkit.Wpf\Kok.Toolkit.Wpf.csproj -c Release -o %PUBLISH_PATH%
-
+dotnet pack .\src\Kok.Toolkit.Avalonia\Kok.Toolkit.Avalonia.csproj -c Release -o %PUBLISH_PATH%
 
 
 pause
